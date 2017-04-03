91f42f5a2ad1925d04b2f4dbc0e047ba019c600a
Yanitsa <yani.valeva@gmail.com>
2017-04-03 20:36:23 +0300

    Create utilities/encryption.js and config/express.js.

---
+++ config/express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+module.exports = (app, config) => {
+
+    app.set('views', path.join(config.rootFolder, '/views'));
+    app.set('view engine', 'hbs');
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use(cookieParser());
+
+    app.use(session({secret: 's3cr3t5tr1ng', resave: false, saveUninitialized: false}));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.use(express.static(path.join(config.rootFolder, 'public')));
+};
+
+
+
