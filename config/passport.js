7cebd422680bebe9416d14864b5c382164df5728
Yanitsa <yani.valeva@gmail.com>
2017-04-04 11:19:06 +0300

    Update models/User.js and config/passport.js.

--- config/passport.js
+++ config/passport.js
@@ -15,3 +15,28 @@ const authenticateUser = (username, password, done) => {
         return done(null, user);
     });
 };
+
+module.exports = () => {
+    passport.use(new LocalPassport({
+        usernameField: 'email',
+        passwordField: 'password'
+    }, authenticateUser));
+
+    passport.serializeUser((user, done) => {
+        if (!user) {
+            return done(null, false);
+        }
+
+        return done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id).then((user) => {
+            if (!user) {
+                return done(null, false)
+            }
+
+            return done(null, user);
+        })
+    })
+};
