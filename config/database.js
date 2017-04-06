b04809d95f73673b9f6f700120d011cca801ff76
Yanitsa <yani.valeva@gmail.com>
2017-04-06 19:15:41 +0300

    Update config/database.js and controllers/user.js. Restore gitignore.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 module.exports = (config) => {
     mongoose.connect(config.connectionString);
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,6 @@
+const User = require('mongoose').model('User');
+const encryption = require('./../utilities/encryption');
+
 module.exports = {
     registerGet: (req, res) => {
         res.render('user/register');
