06c20612b80573b1f4b48edd1c3fffa6f1aa9012
stanislavangelkov <hkraziel@gmail.com>
2017-04-11 19:02:52 +0300

    Added Views for delete,edit and details on articles. Added functionality for deleting articles.

--- config/routes.js
+++ config/routes.js
@@ -26,4 +26,6 @@ module.exports = (app) => {
     app.get('/article/edit/:id', articleController.editGet);
     app.post('/article/edit/:id', articleController.editPost);
 
+    app.get('/article/delete/:id', articleController.deleteGet);
+    app.post('/article/delete/:id', articleController.deletePost);
 };
